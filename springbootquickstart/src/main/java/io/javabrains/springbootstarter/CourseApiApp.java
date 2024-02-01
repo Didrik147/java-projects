4053d8b63756d20e3f920968b7faeab5cdbe6a4f
Didrik Roest <didrik.roest@yahoo.com>
2024-02-01 20:48:37 +0100

    Spring Boot Quick Start 20 - done

--- springbootquickstart/src/main/java/io/javabrains/springbootstarter/CourseApiApp.java
+++ springbootquickstart/src/main/java/io/javabrains/springbootstarter/CourseApiApp.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 // Course tutorial: https://www.youtube.com/watch?v=xpg29DL-ozk&list=PLqq-6Pq4lTTbx8p2oCgcAQGQyqN8XeA1x&index=17
 // Currently at lesson 21, time 00:00
 
+
 @SpringBootApplication
 public class CourseApiApp {
   public static void main(String[] args) {
