c64e3c043feea4ec4f6836ea9dab9cb33a2ecef4
Didrik Roest <didrik.roest@yahoo.com>
2024-08-16 21:53:14 +0200

    JDBC

--- telusko/src/Demo.java
+++ telusko/src/Demo.java
@@ -1,7 +1,20 @@
+import java.sql.*;
+import static java.lang.System.*;
+
 public class Demo {
-    public static void main(String[] args){
-        String sql = "SELECT name FROM product WHERE id=8";
+    public static void main(String[] args) throws Exception {
 
+        String sql = "SELECT name FROM product WHERE id=3";
+        String url = "jdbc:postgresql://localhost:5432/telusko";
+        String username = "postgres";
+        String password = "aldri";
 
+        Connection con = DriverManager.getConnection(url, username, password);
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(sql);
+
+        rs.next();
+        String name = rs.getString(1);
+        out.println(name);
   }
 }
