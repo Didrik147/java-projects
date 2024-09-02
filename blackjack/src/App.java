5a90b85bff29ca0409b337d80f896d8a314382a7
Didrik Roest <didrik.roest@yahoo.com>
2024-09-02 19:57:50 +0200

    6:40

---
+++ blackjack/src/App.java
@@ -0,0 +1,8 @@
+// Made by using the following tutorial:
+// https://youtu.be/GMdgjaDdOjI?si=SSacktdYqk-_m0si
+
+public class App {
+    public static void main(String[] args) {
+        BlackJack blackJack = new BlackJack();
+    }
+}
