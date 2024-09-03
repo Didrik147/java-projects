142bb8bd7647b32a04a859c5198d94985272bef6
Didrik Roest <didrik.roest@yahoo.com>
2024-09-03 13:43:02 +0200

    9:05

--- blackjack/src/BlackJack.java
+++ blackjack/src/BlackJack.java
@@ -5,12 +5,30 @@ import java.util.Random;
 import javax.swing.*;
 
 public class BlackJack {
+    private class Card {
+        String value;
+        String type;
+
+        // Constructor
+        Card(String value, String type){
+            this.value = value;
+            this.type = type;
+        }
+    }
+
+    ArrayList<Card> deck;
+
     // Constructor
     BlackJack(){
         startGame();
     }
 
     public void startGame(){
-        //
+        // Deck
+        buildDeck();
+    }
+
+    public void buildDeck(){
+        deck = new ArrayList<Card>();
     }
 }
