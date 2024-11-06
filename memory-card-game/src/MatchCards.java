69eaf5025c6681616136b825f943d7f859a8d00b
Didrik Roest <didrik.roest@yahoo.com>
2024-11-06 13:33:09 +0100

    Progress

--- memory-card-game/src/MatchCards.java
+++ memory-card-game/src/MatchCards.java
@@ -13,10 +13,145 @@ public class MatchCards {
             this.cardImageIcon = cardImageIcon;
         }
 
-        // Currently at 6:40
+        public String toString(){
+            return cardName;
+        }
     }
 
+    // track cardNames
+    String[] cardList = {
+        "darkness",
+        "double",
+        "fairy",
+        "fighting",
+        "fire",
+        "grass",
+        "lightning",
+        "metal",
+        "psychic",
+        "water"
+    };
+
+    int rows = 4;
+    int columns = 5;
+
+    int cardWidth = 90;
+    int cardHeight = 128;
+
+    // Create a deck of cards with cardNames and cardImageIcons
+    ArrayList<Card> cardSet;
+    ImageIcon cardBackImageIcon;
+
+
+    int boardWidth = columns * cardWidth; // 5*128 = 640px
+    int boardHeight = rows * cardHeight; // 4*90 = 360px
+
+    JFrame frame = new JFrame("Pokemon Match Cards");
+    JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel();
+    JPanel boardPanel = new JPanel();
+    JPanel restartGamePanel = new JPanel();
+    JButton restartButton = new JButton();
+
+    int errorCount = 0;
+    ArrayList<JButton> board;
+    Timer hideCardTimer;
+    boolean gameReady = false;
+    // currently at 31:52
+
     MatchCards(){
-        System.out.println("match cards");
+        setUpCards();
+        shuffleCards();
+
+        frame.setLayout(new BorderLayout());
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        textLabel.setFont(new Font("Arial", Font.PLAIN, 20));
+        textLabel.setHorizontalAlignment(JLabel.CENTER);
+        textLabel.setText("Errors: " + Integer.toString(errorCount));
+
+        textPanel.setPreferredSize(new Dimension(boardWidth, 30));
+        textPanel.add(textLabel);
+        frame.add(textPanel, BorderLayout.NORTH);
+
+        // Card game board
+        board = new ArrayList<JButton>();
+        boardPanel.setLayout(new GridLayout(rows, columns));
+        for(int i=0; i<cardSet.size(); i++){
+            JButton tile = new JButton();
+            tile.setPreferredSize(new Dimension(cardWidth, cardHeight));
+            tile.setOpaque(true);
+            tile.setIcon(cardSet.get(i).cardImageIcon);
+            tile.setFocusable(false);
+            board.add(tile);
+            boardPanel.add(tile);
+        }
+        frame.add(boardPanel);
+
+        // Restart game button
+        restartButton.setFont(new Font("Arial", Font.PLAIN, 16));
+        restartButton.setText("Restart Game");
+        restartButton.setPreferredSize(new Dimension(boardWidth, 30));
+        restartButton.setFocusable(false);
+        restartGamePanel.add(restartButton);
+        frame.add(restartGamePanel, BorderLayout.SOUTH);
+
+        frame.pack();
+        frame.setVisible(true);
+
+        // Start game
+        hideCardTimer = new Timer(1500, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                hideCards();
+            }
+        });
+        hideCardTimer.setRepeats(false);
+        hideCardTimer.start();
+    }
+
+    void setUpCards(){
+        cardSet = new ArrayList<Card>();
+
+        for(String cardName : cardList){
+            // Load each card image
+            Image cardImg = new ImageIcon(getClass().getResource("./img/" + cardName + ".jpg")).getImage();
+            ImageIcon cardImageIcon = new ImageIcon(cardImg.getScaledInstance(cardWidth, cardHeight, java.awt.Image.SCALE_SMOOTH));
+
+            // Create card object and add to cardSet
+            Card card = new Card(cardName, cardImageIcon);
+            cardSet.add(card);
+        }
+        cardSet.addAll(cardSet);
+
+        // Load the back card image
+        Image cardBackImg = new ImageIcon(getClass().getResource("./img/back.jpg")).getImage();
+        cardBackImageIcon = new ImageIcon(cardBackImg.getScaledInstance(cardWidth, cardHeight, java.awt.Image.SCALE_SMOOTH));
+    }
+
+    void shuffleCards(){
+        System.out.println(cardSet);
+
+        // Shuffle
+        for(int i=0; i<cardSet.size(); i++){
+            // Get random index
+            int j = (int) (Math.random()*cardSet.size());
+            // Swap
+            Card temp = cardSet.get(i);
+            cardSet.set(i, cardSet.get(j));
+            cardSet.set(j, temp);
+        }
+
+        System.out.println(cardSet);
+    }
+
+    void hideCards(){
+        for(int i=0; i<cardSet.size(); i++){
+            board.get(i).setIcon(cardBackImageIcon);
+        }
+        gameReady = true;
     }
 }
--- memory-card/App.java
+++
@@ -1,8 +0,0 @@
-// Made by the following tutorial by Kenny Yip Coding:
-// https://www.youtube.com/watch?v=FxDirbh3tXc
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        MatchCards matchCards = new MatchCards();
-    }
-}
